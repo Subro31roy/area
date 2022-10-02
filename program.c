66e8e4f9eaa7528f2bbbe1344cef4b826881b959
Subro31roy <subrotoroy1031@gmail.com>
2022-10-02 17:29:44 +0530

    frist commit

---
+++ program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a=19, b=12;
+    int area= (0.5)*a*b;
+    int f=4000, g=200, c=3000, d=140, e=10000;
+    int total= e-(f+g+c+d);
+    printf("%d\n %d\n",area,total); 
+
+
+
+
+}
